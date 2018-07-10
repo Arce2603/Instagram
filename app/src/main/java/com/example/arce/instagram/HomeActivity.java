13911b30081fd21ac7cfef6e057e83df82ce9d7d
Arcelia Villaseñor <arce@fb.com>
2018-07-10 13:48:56 -0700

    Before camera

---
+++ app/src/main/java/com/example/arce/instagram/HomeActivity.java
@@ -0,0 +1,98 @@
+package com.example.arce.instagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.arce.instagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    //private static final String imageUrl= "/storage/emulated/0/DCIM/Camera/IMG_20180710_131547.jpg";
+    private EditText descriptionInput;
+    private Button createBtn;
+    private Button refreshBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+/*        descriptionInput = findViewById(R.id.etDesc);
+        createBtn = findViewById(R.id.btnCreate);
+        refreshBtn = findViewById(R.id.btnRefresh);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String description = descriptionInput.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+                //final ParseFile img =
+
+                final File file = new File(imageUrl);
+                final ParseFile parseFile =  new ParseFile(file);
+
+                createPost(description,parseFile,user);
+            }
+        });
+
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadTopPost();
+            }
+        });*/
+
+    }
+
+    private void createPost(String description, ParseFile image, ParseUser user){
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(image);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e== null){
+                        Log.d("HomeActivity","Create post success");
+                }
+                else{
+                    e.printStackTrace();
+                    Log.d("HomeActivity","Create post FAIL");
+                }
+
+            }
+        });
+
+    }
+
+    private  void loadTopPost (){
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e== null){
+                    for(int i = 0; i<objects.size(); i++)
+                        Log.d("HomeActivity","Post("+i+") "+ objects.get(i).getDescription()
+                                + "\n username =  " + objects.get(i).getUser().getUsername());
+                }
+                else{
+                    e.printStackTrace();;
+                }
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/arce/instagram/MainActivity.java
+++ app/src/main/java/com/example/arce/instagram/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.arce.instagram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userInput;
+    private EditText passwordInput;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.etUser);
+        passwordInput = findViewById(R.id.etPassword);
+        loginBtn = findViewById(R.id.btnLogin);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = userInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                login(username,password);
+            }
+        });
+
+    }
+
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null) {
+                    Log.d("LOGIN", "Successfull");
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Log.d("LOGIN", "Failure");
+                    e.printStackTrace();;
+                }
+            }
+        });
+
+
     }
 }
