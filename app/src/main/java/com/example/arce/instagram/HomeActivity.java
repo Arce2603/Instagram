3765103657d40359ad27a0ced9a18ccbae16fbcc
Arcelia Villaseñor <arce@fb.com>
2018-07-12 17:44:14 -0700

    improved UI 2.0

--- app/src/main/java/com/example/arce/instagram/HomeActivity.java
+++ app/src/main/java/com/example/arce/instagram/HomeActivity.java
@@ -17,10 +17,7 @@ import android.widget.Button;
 import com.example.arce.instagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -143,50 +140,6 @@ public class HomeActivity extends AppCompatActivity {
         });
         swipeContainer.setRefreshing(false);
     }
-
-
-    private void createPost(String description, ParseFile image, ParseUser user){
-        final Post newPost = new Post();
-        newPost.setDescription(description);
-        newPost.setImage(image);
-        newPost.setUser(user);
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e== null){
-                        Log.d("HomeActivity","Create post success");
-                }
-                else{
-                    e.printStackTrace();
-                    Log.d("HomeActivity","Create post FAIL");
-                }
-
-            }
-        });
-
-    }
-
-    private  void loadTopPost (){
-        final Post.Query postQuery = new Post.Query();
-        postQuery.getTop().withUser();
-
-        postQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if(e== null){
-                    for(int i = 0; i<objects.size(); i++)
-                        Log.d("HomeActivity","Post("+i+") "+ objects.get(i).getDescription()
-                                + "\n username =  " + objects.get(i).getUser().getUsername());
-                }
-                else{
-                    e.printStackTrace();;
-                }
-
-            }
-        });
-    }
-
 
     // Query messages from Parse so we can load them into the chat adapter
     void refreshMessages() {
